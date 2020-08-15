bed4297f87cf005b04f626a5aad8c5067c0b6d42
Juan Pablo Loaiza Ramirez <jploaizar@unal.edu.co>
2020-08-15 15:16:40 -0500

    Plot 1 in the Course Project 1 of Exploratory Data Analysis Course

--- plot1.R
+++ plot1.R
@@ -8,10 +8,12 @@ datahousehold <- read.table(fileName,
                    col.names = dataNames,
                    skip = grep("^[1,2]/2/2007", readLines(fileName)) - 1,
                    nrow = 2879)
+## Creating the histogram
 hist(datahousehold$Global_active_power,
      col = "red",
      main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)",
      ylab = "Frecuency")
+## Coping it to a .png file
 dev.copy(png, file = "plot1.png")
 dev.off()
